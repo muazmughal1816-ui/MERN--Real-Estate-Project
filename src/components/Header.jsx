4c46a261feae7e50295bc85d5cbd11a82b46c627
muazmughal1816-ui <muazmughal1816@gmail.com>
2026-09-08 11:13:19 -0700

    create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header className='border-slate-200 shadow-md mb-1'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to='/'>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap '>
+            <span className='text-slate-500'>Mughal'z</span>
+            <span className='text-slate-700'>Estate</span>
+        </h1>
+        </Link>
+        <form className='bg-slate-100 p-2 rounded-lg flex items-center'>
+            <input className='bg-slate-100 focus:outline-none p-2 rounded-lg w-24 sm:w-64' type="text" placeholder='Search...'/>
+            <FaSearch className='text-slate-600'/>
+        </form>
+        <ul className='flex gap-4'>
+            <Link to='/home'>
+            <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>Home</li>
+            </Link>
+            <Link to='/about'>
+            <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li>
+            </Link>
+            <Link to='/sign-in'>
+            <li className=' text-slate-700 hover:underline cursor-pointer'>Sign in</li>
+            </Link>
+        </ul>
+        </div>
+    </header>
+  )
+}
+
+export default Header
