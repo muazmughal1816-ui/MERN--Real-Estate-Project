970411b9e3b0e25727e891bb07c4622e3df9577c
muazmughal1816-ui <muazmughal1816@gmail.com>
2026-09-17 10:00:21 -0700

    complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,31 @@
 import { FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Header = () => {
     const {currentUser} = useSelector(state => state.user)
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
     //  console.log("Redux ka live data:", currentUser);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    };
+
+    useEffect(() => {
+      const urlParams = new URLSearchParams(location.search);
+      const searchTermFromUrl = urlParams.get('searchTerm');
+      if (searchTermFromUrl) {
+        setSearchTerm(searchTermFromUrl)
+      }
+    }, [location.search]);
+    
+
   return (
     <header className='border-slate-200 shadow-md mb-1'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -14,9 +35,13 @@ const Header = () => {
             <span className='text-slate-700'>Estate</span>
         </h1>
         </Link>
-        <form className='bg-slate-100 p-2 rounded-lg flex items-center'>
-            <input className='bg-slate-100 focus:outline-none p-2 rounded-lg w-24 sm:w-64' type="text" placeholder='Search...'/>
+        <form onSubmit={handleSubmit}
+        className='bg-slate-100 p-2 rounded-lg flex items-center'>
+            <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}
+            className='bg-slate-100 focus:outline-none p-2 rounded-lg w-24 sm:w-64' type="text" placeholder='Search...'/>
+            <button>
             <FaSearch className='text-slate-600'/>
+            </button>
         </form>
         <ul className='flex gap-4'>
             <Link to='/home'>
